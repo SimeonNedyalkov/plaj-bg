f23dc78f3f0684404744d591d6c3b79c3b3331fc
SimeonNedyalkov <simeon_sg@abv.bg>
2025-05-29 11:07:46 +0300

    change of looks

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,17 +4,21 @@ import Shop from "./shopAndParalax/Shop";
 
 export default function Home() {
   return (
-    <div className="shopContainer">
-      <div className="leftSideBar">
-        <LeftSideBar />
-      </div>
+    <div className="homeContainer">
+      <div className="shopContainer">
+        <div className="leftSideBar">
+          <LeftSideBar />
+        </div>
 
-      <div className="middleWrapper">
-        <Shop />
+        <div className="middleWrapper">
+          <Shop />
+        </div>
+        <div className="rightSideBar">
+          <RightSideBar />
+        </div>
       </div>
-      <div className="rightSideBar">
-        <RightSideBar />
-      </div>
+      <img src="/Sea_Transparent_PNG_Image.png" alt="" className="sea" />
+      <img src="/Seagull_PNG_Clip_Art_Image.png" alt="" className="seagull" />
     </div>
   );
 }
