3a4a929fdd5ba85e81ad2f5ef98ec4f4020175cc
SimeonNedyalkov <simeon_sg@abv.bg>
2025-05-29 16:45:28 +0300

    home page finished

--- frontend/src/shop/LeftSidebar.tsx
+++ frontend/src/shop/LeftSidebar.tsx
@@ -3,8 +3,18 @@ import { GrCapacity } from "react-icons/gr";
 import { LuWeight } from "react-icons/lu";
 import { GrStatusPlaceholder } from "react-icons/gr";
 import { SlWrench } from "react-icons/sl";
+import { useState } from "react";
 export default function LeftSideBar() {
   const classicData = arrayWithBeanBagTypes[0];
+  const [selectedColor, setSelectedColor] = useState("#FF5733");
+
+  const availableColors = [
+    "#FF5733",
+    "#4CAF50",
+    "#2196F3",
+    "#FFC107",
+    "#9C27B0",
+  ];
 
   const stats = [
     { name: "Volume", icon: <GrCapacity />, value: classicData.volume ?? 0 },
@@ -19,7 +29,25 @@ export default function LeftSideBar() {
 
   return (
     <div className="leftSideBarContainer">
-      <div className="leftSideBarTop">LeftSideBar</div>
+      <div className="leftSideBarTop">
+        <div className="colorArc">
+          {availableColors.map((color) => (
+            <div
+              key={color}
+              className="colorDot"
+              style={{ backgroundColor: color }}
+              onClick={() => setSelectedColor(color)}
+            />
+          ))}
+        </div>
+        <div
+          className="selectedColorCircle"
+          style={{ backgroundColor: selectedColor }}
+        />
+        <div className="colorArcText">
+          Here you can select the color of your Bean Bag !
+        </div>
+      </div>
       <div className="leftSideBarBottom">
         <div className="beanBagStats">
           {stats.map(({ name, value, icon }) => (
